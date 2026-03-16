34fe9cf9e8dcb458631bebcb8278cef212564d27
vicjuma <vicjuma945@gmail.com>
2026-03-16 23:31:42 +0300

    scripts submission

--- submission/01.sh
+++ submission/01.sh
@@ -1,1 +1,2 @@
 # Write the bitcoin cli command to get the bitcoin node installation version 
+bitcoin-cli -regtest --version
--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,2 @@
 # Write the bitcoin cli command to get the bitcoin node network name
+bitcoin-cli getblockchaininfo | jq -r .chain
